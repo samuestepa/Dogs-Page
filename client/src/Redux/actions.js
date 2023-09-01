69d64a42a85d21568e74441b71ece51cea367d52
samuestepa <68454104+samuestepa@users.noreply.github.com>
2023-09-01 15:56:44 -0500

    redux sin debug

---
+++ client/src/Redux/actions.js
@@ -0,0 +1,173 @@
+import axios from 'axios';
+import { 
+    GET_DOGS,
+    SEARCH_DOGS_BY_NAME,
+    GET_TEMPERAMENTS,
+    FILTER_BY_TEMPERAMENT,
+    FILTER_BY_ORIGIN,
+    ORDER,
+    CREATE_DOG,
+    SET_PAGE,
+    SET_TOTAL_PAGE,
+    SET_LOADING,
+    SET_CLEAN,
+    UPDATE_ORDER,
+    CLEAN_FILTER } from "./actionTypes";
+
+const URL = "http://localhost:3001/dogs";
+
+export const getDogs = () => {
+    return async function (dispatch) {
+        const response = axios.get(URL);
+        const dogs = response.data;
+
+        dispatch({
+            type: GET_DOGS,
+            payload: dogs
+        });
+
+        dispatch(setTotalPage());
+    }
+};
+
+export const searchDogsByName = (name) => {
+    return async function (dispatch) {
+        try {
+            dispatch(setLoading(true));
+            const response = axios.get(URL + `/name/${name}`);
+            const dogByName = response.data;
+    
+            dispatch({
+                type: SEARCH_DOGS_BY_NAME,
+                payload: dogByName
+            });
+            dispatch(setTotalPage());   
+
+        } catch (error) {
+            if(error.response.status === 404)
+            return alert('No dog breeds found with that name')
+        } finally {
+            dispatch(setLoading(false))
+        }
+    }
+};
+
+export const getTemperaments = () => {
+    return async function (dispatch) {
+        try {
+            dispatch(setLoading(true));
+            const response = await axios(URL + "/temperaments");
+            const temperaments = response.data;
+
+            dispatch({
+                type: GET_TEMPERAMENTS,
+                payload: temperaments
+            });
+        } catch (error) {
+            alert('No found temperaments');
+        } finally {
+            dispatch(setLoading(false))
+        }
+    }
+};
+
+export const filterByTemperament = (option) => {
+    return (dispatch) => {
+            dispatch({
+                type: FILTER_BY_TEMPERAMENT,
+                payload: option
+            });
+            dispatch(setTotalPage())
+    }
+};
+
+export const filterByOrigin = (option) => {
+    return (dispatch) => {
+        dispatch({
+            type: FILTER_BY_ORIGIN,
+            payload: option
+        })
+    }
+};
+// define ordenamiento
+export const order = () => ({
+    type: ORDER
+});
+
+export const createDog = (payload) => {
+    return async function (dispatch) {
+        try {
+            const service = await axios.post(URL, payload)
+
+            dispatch({
+                type: CREATE_DOG,
+                payload,
+            });
+
+            if (service.status === 201) {
+                return alert('Succesfully created')
+            }
+        } catch (error) {
+            if (error.response.status === 400) {
+                return alert('Dog breeds name already exists')
+            }
+            if (error.response.status === 404) {
+                return alert('Internal server error')
+            }
+        }
+    }
+};
+
+// Acción para traer definir página actual
+export const setPage = (pageNumber) => ({
+    type: SET_PAGE,
+    payload: pageNumber,
+});
+
+// Acción para traer definir el total de páginas
+export const setTotalPage = () => {
+    return {
+        type: SET_TOTAL_PAGE
+    }
+};
+
+// Acción para actualizar estado de carga
+export const setLoading = (isLoading) => ({
+    type: SET_LOADING,
+    payload: isLoading,
+});
+
+// Acción para actualizar estado de limpieza
+export function setClean(isClean) {
+    return {
+        type: SET_CLEAN,
+        payload: isClean
+    }
+};
+
+// Acción para actualizar ordenamiento
+export const updateSortedList = (sortedList) => ({
+    type: UPDATE_ORDER,
+    payload: sortedList,
+});
+
+// Acción para limpiar filtros
+export function cleanFilter() {
+    return async function (dispatch) {
+        try {            
+            dispatch(setClean(true));
+
+            dispatch({
+                type: CLEAN_FILTER,
+            });
+
+            dispatch(setTotalPage());
+
+            await new Promise(resolve => setTimeout(resolve, 1000))
+
+            dispatch(setClean(false));
+        } catch (error) {
+            console.log(error)
+        }
+    }
+};
---
+++ client/src/Redux/reducer.js
@@ -0,0 +1,147 @@
+import {
+    GET_DOGS,
+    SEARCH_DOGS_BY_NAME,
+    GET_TEMPERAMENTS,
+    FILTER_BY_TEMPERAMENT,
+    FILTER_BY_ORIGIN,
+    ORDER,
+    SET_PAGE,
+    SET_TOTAL_PAGE,
+    SET_LOADING,
+    SET_CLEAN,
+    UPDATE_ORDER,
+    CLEAN_FILTER
+} from "./actionTypes";
+
+const initialState = {
+    allDogs: [],
+    allTemperaments: [],
+    filteredData: [],
+    currentPage: 1,
+    totalPages: 1,
+    isLoading: false,
+    isClean: false,
+    sortOrder: "asc"
+}
+
+
+export default function rootReducer(state = initialState, { type, payload }) {
+    switch (type) {
+        case GET_DOGS: //Reducer para actualizar todos los perros
+            return {
+                ...state,
+                allDogs: payload,
+                filteredData: payload,
+                sortOrder: "asc"
+            };
+        case SEARCH_DOGS_BY_NAME: //Reducer para actualizar resultados de la busqueda
+            return {
+                ...state,
+                filteredData: payload
+            };
+        case GET_TEMPERAMENTS: //Reducer para actualizar todos los temperamentos
+            return {
+                ...state,
+                allTemperaments: payload
+            };
+        case FILTER_BY_TEMPERAMENT: { //Reducer para filtrar por temperamento
+            const filterTemperament = payload;
+            let filteredDogsData;
+
+            if (filterTemperament === 'all') {
+                return {
+                    ...state,
+                    currentPage: 1,
+                    filteredData: state.allDogs,
+                };
+            }
+
+            filteredDogsData = state.allDogs.filter((dog) =>
+                dog.temperament && dog.temperament.includes(filterTemperament)
+            );
+
+            return {
+                ...state,
+                currentPage: 1,
+                filteredData: filteredDogsData,
+                sortOrder: "asc"
+            };
+        };
+        case FILTER_BY_ORIGIN: { //Reducer para filtrar por origen
+            const filterOrigin = payload;
+            let filteredDogsData;
+
+            if (filterOrigin === 'all') {
+                return {
+                    ...state,
+                    currentPage: 1,
+                    filteredData: state.allDogs,
+                };
+            }
+            if (filterOrigin === 'api') {
+                filteredDogsData = state.allDogs.filter((dog) => !dog.created);
+            } else if (filterOrigin === 'db') {
+                filteredDogsData = state.allDogs.filter((dog) => dog.created);
+            } else {
+                filteredDogsData = [];
+            }
+
+            return {
+                ...state,
+                currentPage: 1,
+                filteredData: filteredDogsData,
+                sortOrder: "asc"
+            };
+        };
+        case ORDER: { //Reducer para definir orden
+            const newSortOrder = state.sortOrder === "asc" ? "desc" : "asc";
+            const sortedList = [...state.filteredData];
+            sortedList.sort((a, b) => {
+                if (newSortOrder === "asc") {
+                    return a.name.localeCompare(b.name);
+                } else {
+                    return b.name.localeCompare(a.name);
+                }
+            });
+
+            return {
+                ...state,
+                sortOrder: newSortOrder,
+                filteredData: sortedList,
+            };
+        };
+        case SET_PAGE: //Reducer para actualizar página actual
+            return {
+                ...state,
+                currentPage: payload
+            };
+        case SET_TOTAL_PAGE: //Reducer para actualizar total de páginas
+            return {
+                ...state,
+                totalPages: Math.ceil(state.filteredData.length / 8)
+            };
+        case SET_LOADING: //Reducer para actualizar estado de carga
+            return {
+                ...state,
+                isLoading: payload
+            };
+        case SET_CLEAN: //Reducer para actualizar estado de limpieza
+            return {
+                ...state,
+                isClean: payload
+            };
+        case UPDATE_ORDER: //Reducer para actualizar orden
+            return {
+                ...state,
+                sortedList: payload,
+            };
+        case CLEAN_FILTER: //Reducer para limpiar filtros
+            return {
+                ...state,
+                filteredData: state.allDogs,
+                sortOrder: "asc",
+            };
+        default:
+            return state;
+    }
+}
